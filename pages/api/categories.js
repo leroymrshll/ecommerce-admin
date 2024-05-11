bb8fa847266bd03f60e2ea01d7b7b60516292123
leroymrshll <162900497+leroymrshll@users.noreply.github.com>
2024-05-11 21:08:16 +0800

    Update categories.js

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -6,35 +6,33 @@ export default async function handle(req, res) {
     await mongooseConnect();
     /* await isAdminRequest(req,res); */
 
-    //Fetch all the existing datas (existing categories).
-    if (method === "GET") {
-        res.json(await Category.find().populate("parent"));
-    }
-
-    //Post the database from the input in categoreis page into MONGODB database.
-    if (method === "POST") {
-        const {name, parentCategory, properties} = req.body;
+    if (method === 'GET') {
+        res.json(await Category.find().populate('parent'));
+      }
+    
+      if (method === 'POST') {
+        const {name,parentCategory,properties} = req.body;
         const categoryDoc = await Category.create({
-            name, 
-            parent:parentCategory || undefined,
-            properties,
+          name,
+          parent: parentCategory || undefined,
+          properties,
         });
         res.json(categoryDoc);
-    }
-
-    //Puts the edits made to the categories into MongoDB database.
-    if (method === "PUT") {
-        const {name, parentCategory, properties, _id} = req.body;
-        const categoryDoc = await Category.updateOne({_id}, {
-            name, 
-            parent:parentCategory || undefined,
-            properties,
+      }
+    
+      if (method === 'PUT') {
+        const {name,parentCategory,properties,_id} = req.body;
+        const categoryDoc = await Category.updateOne({_id},{
+          name,
+          parent: parentCategory || undefined,
+          properties,
         });
         res.json(categoryDoc);
-    }
-    if (method === "DELETE") {
+      }
+    
+      if (method === 'DELETE') {
         const {_id} = req.query;
         await Category.deleteOne({_id});
-        res.json("ok");
+        res.json('ok');
+      }
     }
-}
